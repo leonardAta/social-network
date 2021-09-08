11415b151b1cadf9a066f23d08c31e11a3e14023
Ata Tangban <atatangban@live.com>
2021-09-08 23:07:01 +0100

    delete function done

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt")
 
 //update user
 router.put("/:id", async(req, res) => {
-  if(req.body.userId === req.params.id || req.user.isAdmin){
+  if(req.body.userId === req.params.id || req.body.isAdmin){
     if(req.body.password){
       try{
         const salt = await bcrypt.genSalt(12)
@@ -26,6 +26,18 @@ router.put("/:id", async(req, res) => {
   }
 })
 //delete user
+router.delete("/:id", async(req, res) => {
+  if(req.body.userId === req.params.id || req.body.isAdmin){
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      res.status(200).json("Account has been deleted")
+    } catch(err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403).json("You can delete only your account")
+  }
+})
 //get user
 //follow a user
 //unfollow a user
