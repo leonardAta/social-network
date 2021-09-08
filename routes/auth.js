3b2583db1f20836dc9706a616a7603028dcecc7a
Ata Tangban <atatangban@live.com>
2021-09-08 17:34:13 +0100

    login authentication

--- routes/auth.js
+++ routes/auth.js
@@ -25,12 +25,23 @@ router.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
     })
-
+    //save and return responseTHINK
     const user = await newUser.save()
     res.status(200).json(user)
   } catch(err) {
     console.log(err)
   }
 })  
+//login user
+router.post("/login", async (req, res) => {
+  try{
+    const user = await User.findOne({email:req.body.email})
+    !user && res.status(404).send("User not found")
+
+  } catch(err) {
+      console.log(err)
+  }
+})
+
 
 module.exports = router
