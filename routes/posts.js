20a0f776296d20b805f3e19768770a2107d4d17d
Ata Tangban <atatangban@live.com>
2021-09-09 23:25:59 +0100

    create new post updated

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,19 @@
 const router = require("express").Router()
+const Post = require("../models/Post")
+//create post
+router.post("/", async(req, res)=> {
+  const newPost = new Post(req.body)
+  try {
+    const savedPost = await newPost.save()
+    res.status(200).json(savedPost)
+  } catch(err) {
+      res.status(500).json(err)
+  }
+})
+//update post
+//delete a post
+//like a post
+//get a post
+//retrieve timeline
+
+module.exports = router
