bb7d2d52a0bc10cb12dcedc377223a6cadee4105
Ata Tangban <atatangban@live.com>
2021-09-10 22:08:36 +0100

    retrieve all posts/timeline

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,6 @@
 const router = require("express").Router()
+const { json } = require("express")
+const { find } = require("../models/Post")
 const Post = require("../models/Post")
 //create post
 router.post("/", async(req, res)=> {
@@ -54,6 +56,28 @@ router.put("/:id/like", async(req, res) => {
   }
 })
 //get a post
+router.get("/:id", async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post)
+  } catch(err) {
+     res.status(500).json(err)
+  }
+})
 //retrieve timeline
+router.get("/timeline/all", async(req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId)
+    const userPosts = await Post.find({ userId: currentUser._id })
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId })
+      })
+    )
+    res.json(userPosts.concat(...friendPosts))
+  } catch(err) {
+      res.status(500).json(err)
+  }
+})
 
 module.exports = router
