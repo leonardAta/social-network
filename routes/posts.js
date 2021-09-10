a79619202d90f351c513a21f3613448f60906ad7
Ata Tangban <atatangban@live.com>
2021-09-10 16:34:37 +0100

    update and delete post updated

--- routes/posts.js
+++ routes/posts.js
@@ -11,7 +11,33 @@ router.post("/", async(req, res)=> {
   }
 })
 //update post
+router.put("/:id", async(req, res)=> {
+  try {
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId) {
+      await post.updateOne({$set:req.body})
+      res.status(200).json("the post has been updated")
+    } else {
+        res.status(403).json("you can only update your own post")
+    }
+  } catch(err) {
+      res.status(500).json(err)
+  }
+})
 //delete a post
+router.delete("/:id", async(req, res)=> {
+  try {
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId) {
+      await post.deleteOne()
+      res.status(200).json("the post has been deleted")
+    } else {
+        res.status(403).json("you can only delete your own post")
+    }
+  } catch(err) {
+      res.status(500).json(err)
+  }
+})
 //like a post
 //get a post
 //retrieve timeline
