d12a5a8e2e558e003242a5654b73944524ab9729
Ata Tangban <atatangban@live.com>
2021-09-12 17:37:16 +0100

    api backend done/follow functioncorrected

--- routes/users.js
+++ routes/users.js
@@ -55,8 +55,8 @@ router.put("/:id/follow", async(req, res) => {
         const user = await User.findById(req.params.id)
         const currentUser = await User.findById(req.body.userId)
         if(!user.followers.includes(req.body.userId)) {
-          await user.updateOne({ $pull:{ followers: req.body.userId }})
-          await currentUser.updateOne({ $pull: { following: req.params.id }})
+          await user.updateOne({ $push:{ followers: req.body.userId }})
+          await currentUser.updateOne({ $push: { following: req.params.id }})
           res.status(200).json("user has been followed")
         } else {
           res.status(403).json("You already follow this user")
