d595cb14c4551b2f4e2e925c35e9557a8b601ee1
Ata Tangban <atatangban@live.com>
2021-10-07 17:44:59 +0100

    route update

--- routes/posts.js
+++ routes/posts.js
@@ -65,19 +65,45 @@ router.get("/:id", async(req, res) => {
   }
 })
 //retrieve timeline
-router.get("/timeline/all", async(req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId)
-    const userPosts = await Post.find({ userId: currentUser._id })
+    const currentUser = await User.findById(req.params.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.following.map((friendId) => {
-        return Post.find({ userId: friendId })
+        return Post.find({ userId: friendId });
       })
-    )
-    res.json(userPosts.concat(...friendPosts))
-  } catch(err) {
-      res.status(500).json(err)
+    );
+    res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
   }
-})
+});
+
+//get user's posts
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({username:req.params.username})
+    const posts = await Post.find({ userId:user._id })
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get("/timeline/all", async (req, res) => {
+//   try {
+//     const currentUser = await User.findById(req.body.userId);
+//     const userPosts = await Post.find({ userId: currentUser._id });
+//     const friendPosts = await Promise.all(
+//       currentUser.following.map((friendId) => {
+//         return Post.find({ userId: friendId });
+//       })
+//     );
+//     res.status(200).json(userPosts.concat(...friendPosts))
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router
